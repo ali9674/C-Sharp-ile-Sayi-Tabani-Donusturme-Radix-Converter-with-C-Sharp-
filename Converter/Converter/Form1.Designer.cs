31a5835b8d77762405ff2c92b52cfc27f92bebe3
ali9674 <ali_123-fb@hotmail.com>
2023-03-09 19:20:35 +0300

    Radix Converter
    Sayı Tabanı Dönüştürme

---
+++ Converter/Converter/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.basamak1 = new System.Windows.Forms.ComboBox();
+            this.basamak2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(388, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // basamak1
+            // 
+            this.basamak1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.basamak1.FormattingEnabled = true;
+            this.basamak1.Items.AddRange(new object[] {
+            "10\'lu(Decimal)",
+            "16\'lı(HexaDecimal)",
+            "8\'li(Octal)",
+            "2\'li(Binary)"});
+            this.basamak1.Location = new System.Drawing.Point(118, 193);
+            this.basamak1.Name = "basamak1";
+            this.basamak1.Size = new System.Drawing.Size(192, 26);
+            this.basamak1.TabIndex = 2;
+            this.basamak1.SelectedIndexChanged += new System.EventHandler(this.basamak1_SelectedIndexChanged);
+            // 
+            // basamak2
+            // 
+            this.basamak2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.basamak2.FormattingEnabled = true;
+            this.basamak2.Items.AddRange(new object[] {
+            "10\'lu(Decimal)",
+            "16\'lı(HexaDecimal)",
+            "8\'li(Octal)",
+            "2\'li(Binary)"});
+            this.basamak2.Location = new System.Drawing.Point(379, 195);
+            this.basamak2.Name = "basamak2";
+            this.basamak2.Size = new System.Drawing.Size(163, 26);
+            this.basamak2.TabIndex = 3;
+            this.basamak2.SelectedIndexChanged += new System.EventHandler(this.basamak2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Chartreuse;
+            this.label1.Location = new System.Drawing.Point(74, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1.SAYİ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Chartreuse;
+            this.label2.Location = new System.Drawing.Point(322, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2.SAYİ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(322, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "to";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Chartreuse;
+            this.label4.Location = new System.Drawing.Point(5, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Basamak değeri";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(707, 378);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(62, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(547, 122);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 67);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(12, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Date";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(781, 442);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.basamak2);
+            this.Controls.Add(this.basamak1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox basamak1;
+        private System.Windows.Forms.ComboBox basamak2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Converter/Converter/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void basamak1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        public string dec_to_bin(int sayi)
+        {
+            string gecici2 = null;
+            string str = null;
+            int  bolum = 0, kalan = 0;
+
+            while (sayi > 1)
+            {
+                kalan = sayi % 2;
+                str = str + kalan.ToString();
+                sayi = sayi / 2;
+
+            }
+            str = str + sayi.ToString();
+            Char[] temp = new char[str.Length];
+            int i = str.Length - 1; ;
+            for (int j = 0; j < str.Length; j++)
+            {
+
+                temp[j] = str[i];
+                i--;
+            }
+
+            for (int j = 0; j < temp.Length; j++)
+                gecici2 = gecici2 + temp[j];
+            return gecici2;
+
+        }
+        private string  dec_to_oct(int sayi)
+        {
+            string gecici=null;
+            string str = null;
+            int bolum = 0, kalan = 0;
+
+            while (sayi > 7)
+            {
+                kalan = sayi % 8;
+                str = str + kalan.ToString();
+                sayi = sayi / 8;
+
+            }
+            str = str + sayi.ToString();
+
+            Char[] temp = new char[str.Length];
+            int i = str.Length - 1; ;
+            for (int j = 0; j < str.Length; j++)
+            {
+
+                temp[j] = str[i];
+                i--;
+            }
+
+            for (int j = 0; j < temp.Length; j++)
+                gecici = gecici + temp[j];
+           
+            return gecici;
+        }
+        public string dec_to_Hex(string sayi)
+        {
+            string gecici4 = null;
+            bool flag = false;
+            int temp2 = int.Parse(sayi);
+
+            string str = null;
+            int bolum = 0;string kalan = "0";
+
+            while (temp2 > 16)
+            {
+                kalan = (temp2 % 16).ToString();
+                kalan = HexFonk(kalan);
+                str = str + kalan.ToString();
+                temp2 = temp2 / 16;
+                flag = true;             //str 8FB
+            }
+            (sayi) = temp2.ToString();
+            if (flag == false)
+            {
+                sayi = HexFonk(sayi.ToString());
+                return sayi;
+
+            }
+            sayi = HexFonk(sayi.ToString());
+            str = str + sayi.ToString();
+
+            Char[] temp = new char[str.Length];
+            int i = str.Length - 1; ;
+            for (int j = 0; j < str.Length; j++)
+            {
+
+                temp[j] = str[i];
+                i--;
+            }
+
+            for (int j = 0; j < temp.Length; j++)
+                gecici4+= temp[j]; 
+            return gecici4;
+
+        }
+        private string  HexFonk(string kalan)
+        {
+            if (kalan == "10")kalan = "A";
+            if (kalan == "11") kalan = "B";
+            if (kalan == "12") kalan = "C";
+            if (kalan == "13") kalan = "D";
+            if (kalan == "14")kalan= "E";   
+                if (kalan == "15") kalan = "F";
+
+            return kalan;
+        }
+        
+       
+
+    
+
+
+        private string bin_to_dec(string s)
+        {
+            string gecici3 = null; ;
+            int toplam = 0, carpan = 1;
+            for (int i = s.Length; i >= 1; i--)
+            {
+                toplam += int.Parse(s[i - 1].ToString()) * carpan;
+                carpan = carpan * 2;
+            }
+            gecici3 = toplam.ToString();
+            return gecici3;
+        }
+        private string oct_to_dec(string s)
+        {
+            string gecici3 = null; ;
+            int toplam = 0, carpan = 1;
+            for (int i = s.Length; i >= 1; i--)
+            {
+                toplam += int.Parse(s[i - 1].ToString()) * carpan;
+                carpan = carpan * 8;
+            }
+            gecici3 = toplam.ToString();
+            return gecici3;
+        }
+        private string hex_to_dec(string s)
+        {
+            string gecici3 = null; ;
+            int toplam = 0, carpan = 1;
+            for (int i = s.Length; i >= 1; i--)
+            {
+                toplam += int.Parse(s[i - 1].ToString()) * carpan;
+                carpan = carpan * 16;
+            }
+            gecici3 = toplam.ToString();
+            return gecici3;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();textBox2.Clear();basamak1.ResetText(); basamak2.ResetText();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (basamak1.Text == "10'lu(Decimal)" && basamak2.Text == "2'li(Binary)")
+            {
+                textBox2.Clear();
+                textBox2.Text = dec_to_bin(int.Parse(textBox1.Text));
+            }
+            else if (basamak1.Text == "10'lu(Decimal)" && basamak2.Text == "8'li(Octal)")
+            {
+                textBox2.Clear();
+                textBox2.Text = dec_to_oct(int.Parse(textBox1.Text));
+
+            }
+            else if (basamak1.Text == "10'lu(Decimal)" && basamak2.Text == "16'lı(HexaDecimal)")
+            {
+                textBox2.Clear();
+                textBox2.Text = dec_to_Hex(textBox1.Text);
+
+            }
+            else if (basamak1.Text == "2'li(Binary)" && basamak2.Text == "16'lı(HexaDecimal)")
+            {
+
+                if (checkfonk(textBox1.Text, 2) == 1)
+                    textBox2.Text = dec_to_Hex(bin_to_dec(textBox1.Text));
+                else { MessageBox.Show("Geçersiz Karakter"); textBox2.Clear(); }
+            }
+            else if (basamak1.Text == "2'li(Binary)" && basamak2.Text == "8'li(Octal)")
+            {
+                if (checkfonk(textBox1.Text, 2) == 1)
+                    textBox2.Text = dec_to_oct(int.Parse(bin_to_dec(textBox1.Text)));
+                else { MessageBox.Show("Geçersiz Karakter"); textBox2.Clear(); }
+            }
+            else if (basamak1.Text == "2'li(Binary)" && basamak2.Text == "10'lu(Decimal)")
+            {
+                if (checkfonk(textBox1.Text, 2) == 1)
+                    textBox2.Text = bin_to_dec(textBox1.Text);
+                else { MessageBox.Show("Geçersiz Karakter"); textBox2.Clear(); }
+            }
+            else if (basamak1.Text == "8'li(Octal)" && basamak2.Text == "10'lu(Decimal)")
+            {
+                if (checkfonk(textBox1.Text, 8) == 1)
+                    textBox2.Text = oct_to_dec(textBox1.Text);
+                else { MessageBox.Show("Geçersiz Karakter"); textBox2.Clear(); }
+            }
+            else if (basamak1.Text == "8'li(Octal)" && basamak2.Text == "16'lı(HexaDecimal)")
+            {
+                if (checkfonk(textBox1.Text, 8) == 1)
+                    textBox2.Text = dec_to_Hex(oct_to_dec(textBox1.Text));
+                else { MessageBox.Show("Geçersiz Karakter"); textBox2.Clear(); }
+            }
+            else if (basamak1.Text == "8'li(Octal)" && basamak2.Text == "2'li(Binary)")
+            {
+                if (checkfonk(textBox1.Text, 8) == 1)
+                    textBox2.Text = dec_to_bin(int.Parse(oct_to_dec(textBox1.Text)));
+                else { MessageBox.Show("Geçersiz Karakter"); textBox2.Clear(); }
+            }
+            else if (basamak1.Text == "16'lı(HexaDecimal)" && basamak2.Text == "10'lu(Decimal)")
+            {
+                textBox2.Text = hex_to_dec(textBox1.Text);
+            }
+            else if (basamak1.Text == "16'lı(HexaDecimal)" && basamak2.Text == "8'li(Octal)")
+            {
+                textBox2.Text = dec_to_oct(int.Parse(hex_to_dec(textBox1.Text)));
+            }
+            else if (basamak1.Text == "16'lı(HexaDecimal)" && basamak2.Text == "2'li(Binary)")
+            {
+                textBox2.Text = dec_to_bin(int.Parse(hex_to_dec(textBox1.Text)));
+            }
+            else if (basamak1.Text == "8'li(Octal)" && basamak2.Text == "8'li(Octal)")
+            {
+                if (checkfonk(textBox1.Text, 8) == 1) textBox2.Text = textBox1.Text; else MessageBox.Show("Geçersiz Karakter!");
+            }
+            else if (basamak1.Text == "2'li(Binary)" && basamak2.Text == "2'li(Binary)")
+            {
+                if (checkfonk(textBox1.Text, 2) == 1) textBox2.Text = textBox1.Text; else MessageBox.Show("Geçersiz Karakter!");
+            }
+            else if (basamak1.Text == "10'lu(Decimal)" && basamak2.Text == "10'lu(Decimal)")
+            {textBox2.Text = textBox1.Text;}
+            else if (basamak1.Text == "16'lı(HexaDecimal)" && basamak2.Text == "16'lı(HexaDecimal)")
+            { textBox2.Text = textBox1.Text; }
+
+
+
+        }
+        public int checkfonk(string a, int taban)
+        { int sayac= 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Convert.ToInt16(a[i].ToString() ) < taban)
+                { sayac++;  }
+
+            }
+            if (sayac == a.Length) return 1;
+            else return 0;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            label5.Text = dt.ToString("f");          
+
+        }
+
+        private void basamak2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
